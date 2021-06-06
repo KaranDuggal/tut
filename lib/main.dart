306f34bf22f92bd75dd10821de2c2cd0e9851b98
Karan duggaal <duggalkaran400@gmail.com>
2021-06-06 20:25:55 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -13,88 +13,94 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('My App'),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              child: Center(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Colors.black,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
                   child: Container(
-                // color: Colors.green,
-                width: 100,
-                height: 100,
-                child: Text('I am a box',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.red, Colors.yellow]),
-                    // shape: BoxShape.circle
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 10,
-                          offset: Offset(2.0, 5.0))
-                    ]),
-              )),
-            ),
-            Container(
-              child: Center(
+                    // color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    child: Text('I am a box',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        gradient:
+                            LinearGradient(colors: [Colors.red, Colors.yellow]),
+                        // shape: BoxShape.circle
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 10,
+                              offset: Offset(2.0, 5.0))
+                        ]),
+                  ),
+                ),
+                Container(
                   child: Container(
-                // color: Colors.green,
-                width: 100,
-                height: 100,
-                child: Text('I am a box',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.blue, Colors.grey]),
-                    // shape: BoxShape.circle
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 10,
-                          offset: Offset(2.0, 5.0))
-                    ]),
-              )),
-            ),
-            Container(
-              child: Center(
+                    // color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    child: Text('I am a box',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        gradient:
+                            LinearGradient(colors: [Colors.blue, Colors.grey]),
+                        // shape: BoxShape.circle
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 10,
+                              offset: Offset(2.0, 5.0))
+                        ]),
+                  ),
+                ),
+                Container(
                   child: Container(
-                // color: Colors.green,
-                width: 100,
-                height: 100,
-                child: Text('I am a box',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.purple, Colors.amberAccent]),
-                    // shape: BoxShape.circle
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 10,
-                          offset: Offset(2.0, 5.0))
-                    ]),
-              )),
+                    // color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    child: Text('I am a box',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        gradient: LinearGradient(
+                            colors: [Colors.purple, Colors.amberAccent]),
+                        // shape: BoxShape.circle
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 10,
+                              offset: Offset(2.0, 5.0))
+                        ]),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ));
   }
 }
